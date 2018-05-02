1c5f6b6da1979d37b766c1c59d42f206cb0ae46f
Michael Salvatore <msalvatore22@gmail.com>
2018-05-02 12:29:06 -0400

    migrated my users/posts tables

--- db/migrate/20180502154430_create_posts_table.rb
+++ db/migrate/20180502154430_create_posts_table.rb
@@ -1,4 +1,11 @@
 class CreatePostsTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.string :content
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
   end
 end
